52fb52ad0cf8bee361d403b50ce744d81e7051d7
Anupam Sarfare <anupamsarfare@gmail.com>
2015-08-25 12:20:36 +0530

    drawer

--- app/src/main/java/com/example/sandeepbhambad/easysolutiondemo/activities/Splash.java
+++ app/src/main/java/com/example/sandeepbhambad/easysolutiondemo/activities/Splash.java
@@ -20,11 +20,11 @@ public class Splash extends Activity {
         Thread timerThread = new Thread() {
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                    Intent intent = new Intent(Splash.this, Register.class);
+                    Intent intent = new Intent(Splash.this, Home.class);
                     startActivity(intent);
                 }
             }
